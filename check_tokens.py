2a6ac23e2ad11aecf7916e2a4f7b25ff710d3631
karamelniycoder <109586521+karamelniycoder@users.noreply.github.com>
2023-03-16 21:32:11 +0700

    updated link

--- check_tokens.py
+++ check_tokens.py
@@ -5,20 +5,27 @@ import asyncio
 async def send_req(addr):
     async with ClientSession() as session:
         try:
-            r = await session.get(f'https://arbitrum.foundation/_next/data/00x36Jbpql9BgTwp8Cnpi/eligibility.json?address={addr.lower()}')
+            r = await session.get(f'https://arbitrum.foundation/_next/data/lB0zTjQwwi0nJOMcTT0Td/eligibility.json?address={addr.lower()}')
+            # print(await r.text())
             r_json = await r.json()
             if r_json == {'pageProps': {'__N_REDIRECT': '/', '__N_REDIRECT_STATUS': 307}, '__N_SSP': True}:
                 print(f'[{addr}] account not exist')
             elif r_json['pageProps']['isEligible'] == True:
-                print(f'[{addr}] tokens: {r_json["pageProps"]["eligibility"]["tokens"]}')
+                tokens = r_json["pageProps"]["eligibility"]["tokens"]
+                print(f'[{addr}] tokens: {tokens}')
+                return int(tokens)
             else:
                 print(f'[{addr}] tokens: 0')
         except Exception as err:
             print(f'[{addr}] error: {err}')
 
 async def manager(addrs):
+    total_tokens = 0
     for f in asyncio.as_completed([send_req(addr) for addr in addrs]):
         res = await f
+        if type(res) == int:
+            total_tokens += res
+    print(f' --- Total tokens: {total_tokens} ---')
 
 if __name__ == '__main__':
     # print('input file with addresses: ')
